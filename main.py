079dce8cf0d3de5b73128eadafeeb4a2926d407e
Bohdan Vodianyk <bohdanvodianyk@gmail.com>
2024-05-12 19:31:28 +0200

    initial commit, MistoLine basic

---
+++ main.py
@@ -0,0 +1,41 @@
+from diffusers import ControlNetModel, StableDiffusionXLControlNetPipeline, AutoencoderKL
+from diffusers.utils import load_image
+from PIL import Image
+import torch
+import numpy as np
+import cv2
+
+# prompt = "aerial view, a futuristic research complex in a bright foggy jungle, hard lighting"
+# prompt = "photo of elephant 8k high definition in jungle"
+prompt = "Create a photo-realistic image of an old town in steampunk style. Include Victorian architecture with brass pipes, gears, and steam vents. Capture the scene at dusk to highlight metallic textures."
+negative_prompt = 'low quality, bad quality, sketches, blurry, ugly, duplicate, poorly drawn, deformed, mosaic'
+
+# image = load_image("https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png")
+image = load_image("town.jpg")
+controlnet_conditioning_scale = 0.5
+
+controlnet = ControlNetModel.from_pretrained(
+    "diffusers/controlnet-canny-sdxl-1.0",
+    torch_dtype=torch.float16
+)
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    controlnet=controlnet,
+    vae=vae,
+    torch_dtype=torch.float16,
+)
+pipe.enable_model_cpu_offload()
+
+image = np.array(image)
+image = cv2.Canny(image, 100, 200)
+image = image[:, :, None]
+image = np.concatenate([image, image, image], axis=2)
+image = Image.fromarray(image)
+
+images = pipe(
+    prompt, negative_prompt=negative_prompt, image=image, controlnet_conditioning_scale=controlnet_conditioning_scale,
+    ).images
+
+images[0].save(f"town_new.png")
+
