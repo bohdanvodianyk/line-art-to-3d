00e5ec53e980333585d04390c72b4cf15d5f2188
Bohdan Vodianyk <bohdanvodianyk@gmail.com>
2024-05-12 21:24:34 +0200

    MistoLine main code for generation

--- MistoLineGenerator.py
+++ MistoLineGenerator.py
@@ -7,11 +7,11 @@ import cv2
 
 # prompt = "aerial view, a futuristic research complex in a bright foggy jungle, hard lighting"
 # prompt = "photo of elephant 8k high definition in jungle"
-prompt = "Create a photo-realistic image of an old town in steampunk style. Include Victorian architecture with brass pipes, gears, and steam vents. Capture the scene at dusk to highlight metallic textures."
+prompt = "Generate an anime-style image of a grand pirate ship with exaggerated proportions and vibrant colors. Feature ornate sails and a tumultuous sea, conveying action and adventure."
 negative_prompt = 'low quality, bad quality, sketches, blurry, ugly, duplicate, poorly drawn, deformed, mosaic'
 
 # image = load_image("https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png")
-image = load_image("town.jpg")
+image = load_image("input-lineart/ship.jpg")
 controlnet_conditioning_scale = 0.5
 
 controlnet = ControlNetModel.from_pretrained(
@@ -37,5 +37,5 @@ images = pipe(
     prompt, negative_prompt=negative_prompt, image=image, controlnet_conditioning_scale=controlnet_conditioning_scale,
     ).images
 
-images[0].save(f"town_new.png")
+images[0].save(f"output-lineart/ship_new.png")
 
